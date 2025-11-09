7034edc9a423d990a8fe7b16ca35bba6f785e2d5
WajahatJunaidKhan <123405108+WajahatJunaidKhan@users.noreply.github.com>
2025-11-09 18:34:11 +0500

    Update db_init.sql

--- db_init.sql
+++ db_init.sql
@@ -3,13 +3,11 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS repositories (
     repo_id TEXT PRIMARY KEY,
     full_name TEXT NOT NULL,
-    name TEXT NOT NULL,
-    owner_login TEXT NOT NULL,
-    stars BIGINT NOT NULL,
+    stars INT,
     url TEXT,
-    metadata JSONB,
-    last_scraped TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+    last_scraped TIMESTAMP
 );
+
 
 CREATE TABLE IF NOT EXISTS repo_star_history (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
